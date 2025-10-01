1b5938e8937e4ea5e73d78124fb21a8b8eb555aa
Michelle <michellelieng2020@gmail.com>
2025-10-01 23:08:17 +1000

    try serpapi api to scrape the web

---
+++ try_serpapi.py
@@ -0,0 +1,35 @@
+"""
+note: need to do both pip install serapi and pip install google-search-results for it to work
+
+refer to: https://github.com/serpapi/google-search-results-python
+"""
+from pprint import pprint
+from serpapi import GoogleSearch
+from dotenv import load_dotenv
+import os
+import json
+
+load_dotenv()
+
+params = {
+  "engine": "google",
+  "q": "top environmental corporates in australia",
+  "location": "Sydney, New South Wales, Australia",
+  "hl": "en",
+  "gl": "au",
+  "google_domain": "google.com.au",
+  "num": "10",
+  "start": "10",
+  "safe": "active",
+  "api_key": os.getenv("SERP_API_KEY")
+}
+
+search = GoogleSearch(params)
+results = search.get_dict()
+
+with open("output.json", "w") as f:
+    json.dump(results, f, indent=4)
+# pprint(results)
+
+# organic_results = results["organic_results"]
+# pprint(organic_results)
