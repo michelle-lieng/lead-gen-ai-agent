7606fe267080ed2f6af8bd0fa857c254b6c3e928
Michelle <michellelieng2020@gmail.com>
2025-10-02 23:31:20 +1000

    moved these files to learning

--- try_config.py
+++
@@ -1,9 +0,0 @@
-import yaml
-
-# Load config.yaml
-with open("config.yaml", "r") as file:
-    config = yaml.safe_load(file)
-
-# Access values
-print(config)                 # Print the entire config
-print(config.get("postgres_database")) # Example: access a key called "database"
