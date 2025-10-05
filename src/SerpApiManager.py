4613bff9097011e5e9e4d88f65b5ab6d17fcf59f
Michelle <michellelieng2020@gmail.com>
2025-10-05 23:43:54 +1100

    moved serpapi code to a separate module

---
+++ src/SerpApiManager.py
@@ -0,0 +1,39 @@
+import json
+import os
+from dotenv import load_dotenv
+from serpapi import GoogleSearch
+
+load_dotenv()
+
+class SerpApiManager:
+    def __init__(self):
+        pass
+
+    def call_api(self, query: str) -> dict:
+        params = {
+        "engine": "google",
+        "q": query,
+        "location": "Sydney, New South Wales, Australia",
+        "hl": "en",
+        "gl": "au",
+        "google_domain": "google.com.au",
+        "num": "100",
+        "start": "0",
+        "safe": "active",
+        "api_key": os.getenv("SERP_API_KEY")
+        }
+
+        search = GoogleSearch(params)
+        results = search.get_dict()
+        return results.get("organic_results", [])
+    
+    def load_from_json(self, 
+                       json_path: str = 'learning/output.json') -> dict:
+        with open(json_path, 'r') as f:
+            # Load the JSON data from the file
+            results = json.load(f)
+        return results.get("organic_results", [])
+
+if __name__ == "__main__":
+    sm = SerpApiManager()
+    print(sm.load_from_json())
