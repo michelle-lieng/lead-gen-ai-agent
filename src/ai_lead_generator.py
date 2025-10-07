12199bcd624e9fafa9842e304343bcbb09d188ce
Michelle <michellelieng2020@gmail.com>
2025-10-07 23:05:06 +1100

    renamed file names, class names and attribute names for best practice + also moved class initialisation into init

--- src/ai_lead_generator.py
+++ src/ai_lead_generator.py
@@ -2,43 +2,29 @@
 Main file where AI Lead Gen runs
 """
 import asyncio
-from dotenv import load_dotenv
-import yaml
-import logging
 
-from DatabaseManager import DatabaseManager
-from SerpAPIManager import SerpAPIManager
-from InitialLeadAgent import InitialLeadAgent
-
-# Load variables
-with open("config.yaml", "r") as file:
-    config = yaml.safe_load(file)
-
-load_dotenv()
-
-# --- Configure logging ---
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(message)s"
-)
+from .database_manager import DatabaseManager
+from .serpapi_manager import SerpAPIManager
+from .lead_extraction_agent import LeadExtractionAgent
 
 class AILeadGenerator():
-    DatabaseManager = DatabaseManager()
-    SerpAPIManager = SerpAPIManager()
-    InitialLeadAgent = InitialLeadAgent()
-
-    def __init__(self):
-        pass
+    def __init__(self,
+                 database_manager: DatabaseManager = None,
+                 serpapi_manager: SerpAPIManager = None,
+                 initial_lead_agent: LeadExtractionAgent = None):
+        self.db = database_manager if database_manager else DatabaseManager()
+        self.serp = serpapi_manager if serpapi_manager else SerpAPIManager()
+        self.extraction_agent = initial_lead_agent if initial_lead_agent else LeadExtractionAgent()
 
     def setup(self):
         """
         Here we connect to db and set up all the tables.
         """
         # connect to db set in config
-        self.DatabaseManager.connect_to_db()
+        self.db.connect_to_db()
 
         # create all tables in db if not already exist
-        self.DatabaseManager.create_tables()
+        self.db.create_tables()
     
     def collect_initial_urls(self, queries: list):
         """
@@ -49,10 +35,10 @@ class AILeadGenerator():
         the postgres table: "initial_urls".
         """
         for query in queries:
-            # serpapi_results = self.SerpAPIManager.call_api(query=query)
+            # serpapi_results = self.serp.call_api(query=query)
             # temporary to save api costs
-            serpapi_results = self.SerpAPIManager.load_from_json()
-            self.DatabaseManager.upsert_initial_urls(
+            serpapi_results = self.serp.load_from_json()
+            self.db.upsert_initial_urls(
                 query=query,
                 serpapi_results=serpapi_results
             )
@@ -75,24 +61,24 @@ class AILeadGenerator():
 
         Will update the "leads" table.
         """
-        rows = self.DatabaseManager.fetch_unprocessed_urls()
+        rows = self.db.fetch_unprocessed_urls()
         
         # run all lead_extractor calls in parallel
         tasks = [
-            self.InitialLeadAgent.lead_extractor(query, title, snippet, link)
+            self.extraction_agent.lead_extractor(query, title, snippet, link)
             for (row_id, query, title, link, snippet) in rows
         ]
         results = await asyncio.gather(*tasks)
 
         for (row_id, _, _, _, _), (leads, scraped) in zip(rows, results):
-            self.DatabaseManager.upsert_leads(row_id, leads, scraped)
+            self.db.upsert_leads(row_id, leads, scraped)
 
     def end_lead_gen(self):
         """
         Save DB and close connection to DB.
         """
-        self.DatabaseManager.download_csv()
-        self.DatabaseManager.close()
+        self.db.download_csv()
+        self.db.close()
 
     async def main(self):
         # 1. Connect to db + create tables
@@ -109,7 +95,9 @@ class AILeadGenerator():
         self.end_lead_gen()
 
 if __name__ == "__main__":
-    AILeadGenerator = AILeadGenerator()
-    asyncio.run(AILeadGenerator.main())
+    ai_lead_generator = AILeadGenerator()
+    asyncio.run(ai_lead_generator.main())
+
+    # run with python -m src.main
 
         
